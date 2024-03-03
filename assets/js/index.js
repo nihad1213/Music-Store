72dde080e60cb4d2c4dc2b757a6b7b4b786aafbc
dropshit <jabbar.huseynzade@gmail.com>
2024-03-03 19:11:53 +0400

    Navbar is added

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const searchIcon = document.querySelector("#search-icon");
+const searchInput = document.querySelector("#search-input");
+
+searchIcon.addEventListener("click", ()=>{
+   searchInput.classList.toggle("hidden");
+   searchInput.classList.toggle("fadeInDown");
+   // searchInput.classList.toggle("fadeInOut")
+   searchIcon.classList.toggle("active-icon");
+})
