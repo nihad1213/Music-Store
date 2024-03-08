38184f0db3740f6ae48d88b528879e0170bcd7fb
Nihad <nihad.nemetli@gmail.com>
2024-03-09 02:36:07 +0400

    admin

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -32,7 +32,7 @@
 </head>
  
 <body>
-    <form action="validate.php" method="post">
+    <form action="adminpanel.php" method="post">
         <div class="login-box">
             <h1><strong>Admin Login</strong></h1>
  
