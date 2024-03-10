5426a3e4ee5b9aba9a09761935550f91b29e7f5a
Nihad <nihad.nemetli@gmail.com>
2024-03-10 23:33:11 +0400

    little fixes

---
+++ adminpanel/partials/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>
+            © <?php echo date("Y"); ?> Copyright: Rhythm Republic Created by: Nihad Namatli
+        </p>
+    </footer>
+</body>
+</html>
---
+++ adminpanel/partials/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS link starts-->
+    <link rel="stylesheet" href="style/adminpanel.css">
+    <!--CSS link ends-->
+
+    <!--Favicon link starts-->
+    <link rel="shortcut icon" href="../assets/logos/admin-fav.png" type="image/x-icon">
+    <!--Favicon link ends-->
+    <title>Admin Panel</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Admin Manager</a>
+            <a href="">New Products</a>
+            <a href="">Best Seller</a>
+            <a href="">Discounted Albums</a>
+            <a href="">Music Manager</a>
+            <a href="">Poster Manager</a>
+            <a href="">Magazine/Book Manager</a>
+        </nav>
+    </header>
+    <hr>
