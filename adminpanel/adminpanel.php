c10235c8a5468b69332d2bacfa8085f51b723b73
Nihad <nihad.nemetli@gmail.com>
2024-03-10 23:20:33 +0400

    created adminpage.php

---
+++ adminpanel/adminpanel.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS link starts-->
+    <link rel="stylesheet" href="style/adminpanel.css">
+    <!--CSS link ends-->
+
+    <!--Favicon link starts-->
+    <link rel="shortcut icon" href="../assets/logos/admin-fav.png" type="image/x-icon">
+    <!--Favicon link ends-->
+    <title>Admin Panel</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Admin Manager</a>
+            <a href="">New Products</a>
+            <a href="">Best Seller</a>
+            <a href="">Discounted Albums</a>
+            <a href="">Music Manager</a>
+            <a href="">Poster Manager</a>
+            <a href="">Magazine/Book Manager</a>
+        </nav>
+    </header>
+    <hr>
+
+    <main>
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+
+        <div class="indicators">
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Admins</p>
+            </div>
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>New Products</p>
+            </div>
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Best Sellers</p>
+            </div>
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Discounted Albums</p>
+            </div>
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Music Album</p>
+            </div>
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Poster</p>
+            </div>  
+
+            <div class="indicator-items">
+                <h2>0</h2>
+                <p>Magazine/Books</p>
+            </div>
+        </div>
+
+    </main>
+
+    <footer>
+        <p>
+            © <?php echo date("Y"); ?> Copyright: Rhythm Republic Created by: Nihad Namatli
+        </p>
+    </footer>
+</body>
+</html>
