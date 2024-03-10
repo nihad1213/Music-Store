d516ab28719b144b61f7eedae0be9d3b899befc0
Nihad <nihad.nemetli@gmail.com>
2024-03-11 00:17:30 +0400

    data folder completed

---
+++ data/newProducts.sql
@@ -0,0 +1,7 @@
+CREATE TABLE newproducts(
+    productID int NOT NULL PRIMARY KEY,
+    productImage varchar(255) NOT NULL,
+    title varchar(255)  NOT NULL,
+    price float NOT NULL,
+    label varchar(255) NOT NULL
+);
