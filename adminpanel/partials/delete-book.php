02f5ff13333c8a6bf9806a9e6d1447585fab82c3
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:00:25 +0400

    Update delete-book.php

--- adminpanel/partials/delete-book.php
+++ adminpanel/partials/delete-book.php
@@ -13,10 +13,10 @@ $result = mysqli_query($connection, $sql);
 
 if ($result == TRUE) {
     $_SESSION['delete-book'] = "<div style='color: #20914f; margin-left: 25px'>Poster Deleted Succesfully</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/book.php");
+    header("location: http://localhost/Music%-Store/adminpanel/partials/book.php");
 } else {
     $_SESSION['delete-book'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Delete Poster</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/book.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/book.php");
 }
 
 ?>
