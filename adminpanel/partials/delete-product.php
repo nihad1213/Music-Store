afbe989d371ecedc27294ae4d247ba9ee0ef0495
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:01:38 +0400

    Update delete-product.php

--- adminpanel/partials/delete-product.php
+++ adminpanel/partials/delete-product.php
@@ -14,9 +14,9 @@
 
     if ($result == TRUE) {
         $_SESSION['delete-product'] = "<div style='color: #20914f; margin-left: 25px'>Product Deleted Succesfully</div>";
-        header("location: http://localhost/Music%20Store/adminpanel/partials/new-products-page.php");
+        header("location: http://localhost/Music-Store/adminpanel/partials/new-products-page.php");
     } else {
         $_SESSION['delete-product'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Delete Product</div>";
-        header("location: http://localhost/Music%20Store/adminpanel/partials/new-products-page.php");
+        header("location: http://localhost/Music-Store/adminpanel/partials/new-products-page.php");
     }
 ?>
