4693ea21d8657f3f40b4b203d54d770424abd518
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:05:21 +0400

    Update edit-music.php

--- adminpanel/partials/edit-music.php
+++ adminpanel/partials/edit-music.php
@@ -76,7 +76,7 @@
             //Check Image added or not
             if ($upload == FALSE) {
                 $_SESSION['upload-image'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Upload Image</div>";
-                header("location: http://localhost/Music%20Store/adminpanel/partials/edit-music.php");
+                header("location: http://localhost/Music-Store/adminpanel/partials/edit-music.php");
             }
         
         }
@@ -95,10 +95,10 @@
 
         if ($result2 == TRUE) {
             $_SESSION['edit-music'] = "<div style='color: #20914f; margin-left: 25px'>Best Seller Edited Succesfully</div>";
-            header("location: http://localhost/Music%20Store/adminpanel/partials/music.php");
+            header("location: http://localhost/Music-Store/adminpanel/partials/music.php");
         } else {
             $_SESSION['edit-music'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Edit Best Seller</div>";
-            header("location: http://localhost/Music%20Store/adminpanel/partials/music.php");
+            header("location: http://localhost/Music-Store/adminpanel/partials/music.php");
         }
         
     }
