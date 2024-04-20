5d554bb01b793eb0779a8a87afc8bfbd6b984fc2
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:00:52 +0400

    Update delete-music.php

--- adminpanel/partials/delete-music.php
+++ adminpanel/partials/delete-music.php
@@ -13,10 +13,10 @@ $result = mysqli_query($connection, $sql);
 
 if ($result == TRUE) {
     $_SESSION['delete-music'] = "<div style='color: #20914f; margin-left: 25px'>Music Album Deleted Succesfully</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/music.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/music.php");
 } else {
     $_SESSION['delete-music'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Delete Music Album</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/music.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/music.php");
 }
 
 ?>
