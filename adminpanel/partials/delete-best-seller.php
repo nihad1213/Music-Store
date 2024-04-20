10b5d3315808be6863a5a35b9ebcfe67428ee440
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:00:10 +0400

    Update delete-best-seller.php

--- adminpanel/partials/delete-best-seller.php
+++ adminpanel/partials/delete-best-seller.php
@@ -13,10 +13,10 @@ $result = mysqli_query($connection, $sql);
 
 if ($result == TRUE) {
     $_SESSION['delete-best-seller'] = "<div style='color: #20914f; margin-left: 25px'>Best Seller Deleted Succesfully</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/best-seller.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/best-seller.php");
 } else {
     $_SESSION['delete-best-seller'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Delete Best Seller</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/best-seller.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/best-seller.php");
 }
 
 ?>
