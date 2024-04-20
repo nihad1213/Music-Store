9ecf265e5495c6e6b13685dcb44d1ea3f270e2c2
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-21 00:01:03 +0400

    Update delete-poster.php

--- adminpanel/partials/delete-poster.php
+++ adminpanel/partials/delete-poster.php
@@ -13,10 +13,10 @@ $result = mysqli_query($connection, $sql);
 
 if ($result == TRUE) {
     $_SESSION['delete-poster'] = "<div style='color: #20914f; margin-left: 25px'>Poster Deleted Succesfully</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/poster.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/poster.php");
 } else {
     $_SESSION['delete-poster'] = "<div style='color: #FF0000; margin-left: 25px'>Failed to Delete Poster</div>";
-    header("location: http://localhost/Music%20Store/adminpanel/partials/poster.php");
+    header("location: http://localhost/Music-Store/adminpanel/partials/poster.php");
 }
 
 ?>
